a52d9be6584e6649108fa8feed23fc300a654123
SERVER174\Administrator <594097921@qq.com>
2024-03-14 16:31:34 +0800

    Add nulinknodecker.sh

--- nulinknodecker.sh
+++ nulinknodecker.sh
@@ -28,103 +28,100 @@ function check_and_set_alias() {
     fi
 }
 
-# 检查 Docker 是否已经安装11111
-function check_docker_installed() {
-    if ! command -v docker &> /dev/null; then
-        echo "请先安装 Docker，然后再运行此脚本。"
-        exit 1
-    fi
-}
-
 # 节点安装功能
 function install_node() {
-    check_docker_installed
 
-    exists() {
-        command -v "$1" >/dev/null 2>&1
-    }
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
 
-    if exists curl; then
-        echo ''
-    else
-        sudo apt update && sudo apt install curl -y < "/dev/null"
-    fi
+# Generate Geth Account
+wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.23-d901d853.tar.gz
 
-    # Generate Geth Account
-    wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.23-d901d853.tar.gz
+tar -xvzf geth-linux-amd64-1.10.23-d901d853.tar.gz
 
-    tar -xvzf geth-linux-amd64-1.10.23-d901d853.tar.gz
+cd geth-linux-amd64-1.10.23-d901d853/
 
-    cd geth-linux-amd64-1.10.23-d901d853/
-
-    ./geth account new --keystore ./keystore
-    sleep 5
-    echo -e "\e[1;32m \e[0m\e[1;36m${CYAN} 保存好你的地址和密码 ${NC}\e[0m"
-    echo -e "\e[1;32m \e[0m\e[1;36m${CYAN} 保存好你的秘钥和路径 ${NC}\e[0m"
-    sleep 5
-
-    # Pull the latest NuLink image
-    docker pull nulink/nulink:latest
-    cd /root
-    mkdir nulink
-    cp /root/geth-linux-amd64-1.10.23-d901d853/keystore/* /root/nulink
-    chmod -R 777 /root/nulink
-    
-    # Initiate Worker
-    read -r -p "您的NULINK存储密码.请务必记住此密码以供将来访问 : " NULINK_KEYSTORE_PASSWORD
-    sleep 0.5
-    export NULINK_KEYSTORE_PASSWORD=$NULINK_KEYSTORE_PASSWORD
-    sleep 0.5
-    read -r -p "您的员工帐户密码.请务必记住此密码以供将来访问 : " NULINK_OPERATOR_ETH_PASSWORD
-    sleep 1
-    export NULINK_OPERATOR_ETH_PASSWORD=$NULINK_OPERATOR_ETH_PASSWORD
-    sleep 1
-    echo " 您的密钥库路径 "
-    filename=$(basename ~/geth-linux-amd64-1.10.23-d901d853/keystore/*)
-    export filename1=$filename
-    sleep 1
-    echo "复制您的密钥库 "
-    read -p "按Enter键继续..."   # 添加这一行，等待用户按下Enter键
-    evm=$(grep -oP '(?<="address":")[^"]+' ~/geth-linux-amd64-1.10.23-d901d853/keystore/*)
-    wallet='0x'$evm
-    export wallet1=$wallet
-    sleep 1
+./geth account new --keystore ./keystore
+sleep 5
+echo -e "\e[1;32m \e[0m\e[1;36m${CYAN} 保存好你的地址和密码 ${NC}\e[0m"
+echo -e "\e[1;32m \e[0m\e[1;36m${CYAN} 保存好你的秘钥和路径 ${NC}\e[0m"
+sleep 5
 
 
-    # 节点配置部署
-    docker run -it --rm \
-    -p 9151:9151 \
-    -v /root/nulink:/code \
-    -v /root/nulink:/home/circleci/.local/share/nulink \
-    -e NULINK_KEYSTORE_PASSWORD \
-    nulink/nulink nulink ursula init \
-    --signer keystore:///code/$filename1 \
-    --eth-provider https://data-seed-prebsc-2-s2.binance.org:8545 \
-    --network horus \
-    --payment-provider https://data-seed-prebsc-2-s2.binance.org:8545 \
-    --payment-network bsc_testnet \
-    --operator-address $wallet1 \
-    --max-gas-price 10000000000
 
-    #加载节点
-    docker run --restart on-failure -d \
-    --name ursula \
-    -p 9151:9151 \
-    -v /root/nulink:/code \
-    -v /root/nulink:/home/circleci/.local/share/nulink \
-    -e NULINK_KEYSTORE_PASSWORD \
-    -e NULINK_OPERATOR_ETH_PASSWORD \
-    nulink/nulink nulink ursula run --no-block-until-ready
+# Pull the latest NuLink image
 
-    echo '=============================== 安装完成 ==============================='
-    echo 启动节点: docker restart ursula
-    echo 检查日志 :docker logs -f ursula
+docker pull nulink/nulink:latest
+cd /root
+mkdir nulink
+cp /root/geth-linux-amd64-1.10.23-d901d853/keystore/* /root/nulink
+chmod -R 777 /root/nulink
+
+# Initiate Worker
+read -r -p "您的NULINK存储密码.请务必记住此密码以供将来访问 : " NULINK_KEYSTORE_PASSWORD
+sleep 0.5
+export NULINK_KEYSTORE_PASSWORD=$NULINK_KEYSTORE_PASSWORD
+sleep 0.5
+read -r -p "您的员工帐户密码.请务必记住此密码以供将来访问 : " NULINK_OPERATOR_ETH_PASSWORD
+sleep 0.5
+export NULINK_OPERATOR_ETH_PASSWORD=$NULINK_OPERATOR_ETH_PASSWORD
+sleep 0.5
+echo " 您的密钥库路径 "
+filename=$(basename ~/geth-linux-amd64-1.10.23-d901d853/keystore/*)
+export filename1=$filename
+sleep 1
+echo "复制您的密钥库 "
+evm=$(grep -oP '(?<="address":")[^"]+' ~/geth-linux-amd64-1.10.23-d901d853/keystore/*)
+wallet='0x'$evm
+export wallet1=$wallet
+sleep 1
+
+# 节点配置部署
+
+docker run -it --rm \
+-p 9151:9151 \
+-v /root/nulink:/code \
+-v /root/nulink:/home/circleci/.local/share/nulink \
+-e NULINK_KEYSTORE_PASSWORD \
+nulink/nulink nulink ursula init \
+--signer keystore:///code/$filename1 \
+--eth-provider https://data-seed-prebsc-2-s2.binance.org:8545 \
+--network horus \
+--payment-provider https://data-seed-prebsc-2-s2.binance.org:8545 \
+--payment-network bsc_testnet \
+--operator-address $wallet1 \
+--max-gas-price 10000000000
+
+#加载节点
+
+docker run --restart on-failure -d \
+--name ursula \
+-p 9151:9151 \
+-v /root/nulink:/code \
+-v /root/nulink:/home/circleci/.local/share/nulink \
+-e NULINK_KEYSTORE_PASSWORD \
+-e NULINK_OPERATOR_ETH_PASSWORD \
+nulink/nulink nulink ursula run --no-block-until-ready
+
+echo '=============================== 安装完成 ==============================='
+echo 启动节点: docker restart ursula
+echo 检查日志 :docker logs -f ursula
+
 }
 
 # 查看节点日志
 function check_service_status() {
     docker logs -f ursula
 }
+
+
 
 # 主菜单
 function main_menu() {
